568478908833715a75655e68b50576c4076cfc1f
captjay98wsl <captjay98@gmail.com>
2023-05-17 20:18:32 +0100

    using helmet

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import helmet from "helmet";
 import morgan from "morgan";
 import { dbConnect } from "./middleware/db.js"
 import uRouter from "./routes/userRoutes.js"
@@ -10,7 +11,7 @@ import { getCurrentUser, requireAccountType } from "./middleware/auth.js"
 
 const app = express();
 
-
+app.use(helmet)
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
