ca4332724fa68332292574f62f7873f8eb56821b
captjay98Zorin <captjay98@gmail.com>
2023-08-21 03:01:54 +0100

    Updated index

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
-import { dbConnect } from "./middleware/db.js"
-import uRouter from "./routes/userRoutes.js"
-import cRouter from "./routes/coreRoutes.js"
-import aRouter from "./routes/adminRoutes.js"
-import eRouter from "./routes/employerRoutes.js"
-import sRouter from "./routes/seekerRoutes.js"
-import { getCurrentUser, requireAccountType } from "./middleware/auth.js"
+import { dbConnect } from "./middleware/db.js";
+import uRouter from "./routes/userRoutes.js";
+import cRouter from "./routes/coreRoutes.js";
+import aRouter from "./routes/adminRoutes.js";
+import eRouter from "./routes/employerRoutes.js";
+import sRouter from "./routes/seekerRoutes.js";
+import { getCurrentUser, requireAccountType } from "./middleware/auth.js";
 
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(helmet())
-app.use(morgan('combined'));
+app.use(helmet());
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-dbConnect()
-  .then(() => {
-    app.listen(3333, () => console.log('App running on Port 3333'));
-  });
+dbConnect().then(() => {
+  app.listen(port, "0.0.0.0", () => console.log("App running on Port 3333"));
+});
 
-
-app.use("/api/users", uRouter)
-app.use("/api/core", cRouter)
-app.use("/api/admin", getCurrentUser, requireAccountType("admin"), aRouter)
-app.use("/api/employers", getCurrentUser, requireAccountType("employer"), eRouter)
-app.use("/api/seekers", getCurrentUser, requireAccountType("seeker"), sRouter)
-
+app.use("/api/users", uRouter);
+app.use("/api/core", cRouter);
+app.use("/api/admin", getCurrentUser, requireAccountType("admin"), aRouter);
+app.use("/api/employers", getCurrentUser, requireAccountType("employer"), eRouter);
+app.use("/api/seekers", getCurrentUser, requireAccountType("seeker"), sRouter);
