a7dead5060d953f00939298d1e4b25cf23d2ea26
captjay98 <captjay98@gmail.com>
2023-08-24 20:03:55 +0100

    added timestamps

--- models/users.js
+++ models/users.js
@@ -1,15 +1,38 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  account_type: { type: String, enum: ['admin', 'employer', 'seeker']},
-  isActive: { type: Boolean, default: true },
-  createdAt: { type: Date, default: Date.now }
-});
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    account_type: {
+      type: String,
+      enum: ["admin", "employer", "seeker"],
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { TimeStamps: true },
+);
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model("User", userSchema);
